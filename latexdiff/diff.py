3155bae3813090356fd332bd180d94f6a4823cff
jstzwj <1103870790@qq.com>
2025-04-24 17:59:37 +0800

    more options

--- latexdiff/diff.py
+++ latexdiff/diff.py
@@ -49,6 +49,8 @@ def main():
     parser.add_argument('--old', type=str, required=True, help='old tex')
     parser.add_argument('--new', type=str, required=True, help='new tex')
     parser.add_argument('--out', default='diff.tex', type=str, required=False, help='output path')
+    parser.add_argument('--config', default='', type=str, required=False, help='config path')
+    parser.add_argument('--exclude-textcmd', default='', type=str, required=False, help='exclude textcmd')
     args = parser.parse_args()
     print('args:\n' + args.__repr__())
 
@@ -60,7 +62,20 @@ def main():
             new_file.flush()
 
     with open(args.out, 'w') as output_f:
-        cmd = f"latexdiff {old_file.name} {new_file.name}"
+        if len(args.config) == 0:
+            cmd = f"latexdiff"
+        else:
+            if os.path.exists(args.config):
+                cmd = f"latexdiff -c {args.config}"
+            else:
+                if not args.config.startswith("\"") and not args.config.endswith("\""):
+                    cmd = f"latexdiff -c {args.config}"
+                else:
+                    cmd = f"latexdiff -c \"{args.config}\""
+        if args.exclude_textcmd:
+            cmd += f" --exclude-textcmd {args.exclude_textcmd}"
+        cmd += f" {old_file.name} {new_file.name}"
+
         print(cmd)
         p = subprocess.Popen(cmd,
                             stdout=output_f,
